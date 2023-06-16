254defd13084d56dab81a61cf8cd5f3596b8882b
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-15 22:13:54 -0400

    move UI functionality to play.js. follow assignment more closely. refactor.

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 // Part of Snake project
 // Created by Nathan Wiles
 
-// client side code for snake game
+// client side code for connecting to snake game server
 
 // import required modules
 const net = require("net");
-const { stdin, exit } = require("process"); // get stdin from process
+const { exit } = require("process");
 
-// function establishes a connection with the game server
+// function to connect to server
 const connect = function () {
   // create connection object
   const conn = net.createConnection({
@@ -15,12 +15,8 @@ const connect = function () {
     port: 50541, // set appropriate port
   });
 
-  // helper function to send data to server
-  const sendData = (data) => {
-    conn.write(data);
-  };
-
-  conn.setEncoding("utf8"); // interpret data as text
+  // interpret data as text
+  conn.setEncoding("utf8");
 
   // log incoming messages from server
   conn.on("data", (data) => {
@@ -31,38 +27,9 @@ const connect = function () {
   conn.on("connect", () => {
     conn.write("Name: NAW");
     console.log("Successfully connected to game server");
-
-    // set up stdin for user input
-    stdin.setEncoding("utf8");
-    stdin.setRawMode(true);
-
-    // listen for user input
-    stdin.on("data", (data) => {
-      switch (
-        data // switch statement to handle input
-      ) {
-        case "\u0003": // ctrl-c closes connection
-          console.log("User terminated connection");
-          exit();
-        case "w":
-          sendData("Move: up");
-          break;
-        case "a":
-          sendData("Move: left");
-          break;
-        case "s":
-          sendData("Move: down");
-          break;
-        case "d":
-          sendData("Move: right");
-          break;
-        default:
-          console.log("Invalid input:", data);
-          break;
-      }
-    });
   });
 
+  // log message when connection is terminated and exit
   conn.on("end", () => {
     console.log("Disconnected from server");
     exit();
