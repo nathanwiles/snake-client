bad3c6a64065e161a68e9747fd326e69efbd33a0
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-15 21:48:47 -0400

    remove forgotten artifact. tidy add comments

--- play.js
+++ play.js
@@ -1,12 +1,7 @@
 const { connect } = require("./client"); // get connect function from client.js
 
-console.log("Connecting ...");
-
+console.log("Connecting ..."); // log message to show connection is being established
 
 // initiate connection to server
 conn = connect();
 
-//while connection is open, listen for user input
-conn.on("connect", () => {
-  
-});
