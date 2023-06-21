aead61368560ab9ba7d7093ad0abc3c2fae591a3
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-21 19:13:37 -0400

    handleUserInputs to use moves and canned messages remove redundant comments

--- src/input.js
+++ src/input.js
@@ -3,40 +3,37 @@
 
 // Client side: Handles user input.
 
-// import and setup required modules
+// import and deconstruct required modules
 const { exit, stdin } = require("process");
-const { KEYRETURNS, LOGMESSAGE, ENCODING } = require("./constants");
+const { MOVES, CANNEDMESSAGES, LOGMESSAGES, ENCODING } = require("./constants");
 
-// declare connection variable
-let connection;
 
-// fuction to set up user input
-const setupInput = (conn) => {
-  // set connection variable to connection object
-  connection = conn;
-  // setup stdin to handle user input
+
+const setupUserInput = (conn) => {
   stdin.setRawMode(true);
   stdin.setEncoding(ENCODING);
   stdin.resume();
   stdin.on("data", (data) => {
-    handleUserInput(data);
+    handleUserInput(data, conn);
   });
+
   return stdin;
 };
 
 // function to handle user input
-const handleUserInput = (key) => {
+const handleUserInput = (key, conn) => {
+
   // exit game if user presses ctrl + c
   if (key === "\u0003") {
-    console.log(LOGMESSAGE.userTerminated);
+    console.log(LOGMESSAGES.userTerminated);
     exit();
   }
 
-  // send data to server if key is in KEYRETURNS object
-  if (KEYRETURNS[key]) {
-    connection.write(KEYRETURNS[key]);
+  if (MOVES[key] || CANNEDMESSAGES[key]){
+    let message = MOVES[key] || CANNEDMESSAGES[key]; 
+    conn.write(message);
   }
 };
 
-// export setupInput function
-module.exports = { setupInput };
+
+module.exports = { setupUserInput };
