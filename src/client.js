f56e642791eabb2664329bc13b72226ca35517a5
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-21 18:40:11 -0400

    remove redundant comments

--- src/client.js
+++ src/client.js
@@ -6,41 +6,41 @@
 // import required modules
 const net = require("net");
 const { exit } = require("process");
-const { IP, PORT, LOGMESSAGE, ENCODING } = require("./constants");
+const { IP, PORT, LOGMESSAGES, ENCODING } = require("./constants");
 
 // function to connect to server
 const connect = function() {
-  // log message to show connection is being established
-  console.log(LOGMESSAGE.connecting);
+  
+  console.log(LOGMESSAGES.connecting);
+
   // create connection object
   const conn = net.createConnection({
-    host: IP, // change to server ip address if external.
-    port: PORT, // set appropriate port
+    host: IP,
+    port: PORT,
   });
 
-  // interpret data as text
+  // Use ENCODING from constants.js
   conn.setEncoding(ENCODING);
 
-  
   // log message when connection is established
   conn.on("connect", () => {
     conn.write("Name: NAW");
-    console.log(LOGMESSAGE.connected);
+    console.log(LOGMESSAGES.connected);
   });
   
   // log incoming messages from server
   conn.on('data', (data) => {
-    console.log(LOGMESSAGE.fromServer, data);
+    console.log(LOGMESSAGES.fromServer, data);
   });
+
   // log message when connection is terminated and exit
   conn.on("end", () => {
-    console.log(LOGMESSAGE.disconnected);
+    console.log(LOGMESSAGES.disconnected);
     exit();
   });
 
-  // return connection object
+
   return conn;
 };
 
-// export connect function
 module.exports = { connect };
