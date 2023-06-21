03f8195f4b3114db2df3d2cdefd2fc44156155f8
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-21 19:14:49 -0400

    remove redundant comments

--- play.js
+++ play.js
@@ -4,15 +4,13 @@
 // Client side: Initializes snake game
 
 // import required modules
-const { connect } = require("./src/client"); // get connect function from client.js
-const { setupInput } = require("./src/input"); // get setupInput function from input.js
+const { connect } = require("./src/client");
+const { setupUserInput } = require("./src/input"); 
 
-// function to run game
+// connect to server and setup user input
 const run = () => {
-  // call connect function and store connection object in conn variable
   const conn = connect();
-  // call setupInput function and pass connection object as argument
-  setupInput(conn);
+  setupUserInput(conn);
 };
 
-run(); // run game
+run();
